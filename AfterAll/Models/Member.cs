a4a25f93a4e7b617eab0a846a258c81971c3f96e
VirtualProgramer <setnight7@gmail.com>
2019-08-12 13:32:10 +0800

    進入編輯頁面時預設把生日日期填入。

--- AfterAll/Models/Member.cs
+++ AfterAll/Models/Member.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace AfterAll.Models {
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Birth { get; set; }
         public int Phone { get; set; }
         public int AccessLevel { get; set; }
